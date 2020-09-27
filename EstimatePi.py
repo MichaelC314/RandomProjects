515521479a8cbaaad08c006f36dfaaf259b18296
MichaelC314 <67126116+MichaelC314@users.noreply.github.com>
2020-09-27 14:22:36 -0500

    Add files via upload

---
+++ EstimatePi.py
@@ -0,0 +1,17 @@
+# Note: This is only done with numbers between 0 and 1 #
+import random
+
+# Note: The higher the number, the closer to pi it is. #
+def EstimatePi(num):
+    Dot_Total = 0
+    Dots_In_Circle = 0
+    for _ in range(num):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        Distance = x**2 + y**2
+        if Distance <= 1:
+            Dots_In_Circle += 1
+        Dot_Total += 1
+    return 4 * Dots_In_Circle / Dot_Total
+
+print(EstimatePi(1_000_000))
